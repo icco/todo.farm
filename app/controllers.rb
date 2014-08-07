c1eb72601d72945b0b635b8d24b5b2ac4e2c4e95
Nat Welch <nat@natwelch.com>
2014-08-07 12:15:43 -0700

    starts

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,5 +1,17 @@
 BrainChild::App.controllers  do
   get :index do
+    @day = Time.new
     render :index
+  end
+
+  get :day, :with => [:year, :month, :day], :map => "/" do
+    @day = Time.utc(params[:year], params[:month], params[:day])
+    @lines = Line.where(:date => @day)
+
+    render :day
+  end
+
+  post :day, :with => [:year, :month, :day], :map => "/" do
+    p params
   end
 end
