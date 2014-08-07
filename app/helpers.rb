471ce7dfec24f305cf0e581d1098d172f02243cf
Nat Welch <nat@natwelch.com>
2014-08-07 09:36:17 -0700

    basic helpers

--- app/helpers.rb
+++ app/helpers.rb
@@ -1,13 +1,20 @@
 # Helper methods defined here can be accessed in any controller or view in the application
-
 module BrainChild
-  class App
-    module 
-      # def simple_helper_method
-      # ...
-      # end
-    end
+  module Helpers
+    include FuzzyTimeHelper
 
-    helpers 
+    def mkd text
+      markdown = Redcarpet::Markdown.new(
+        Redcarpet::Render::HTML,
+        autolink: true,
+        tables: true,
+        no_intra_emphasis: true,
+        strikethrough: true,
+        fenced_code_blocks: true)
+
+        markdown.render(text.to_s)
+    end
   end
 end
+
+BrainChild::App.helpers BrainChild::Helpers
