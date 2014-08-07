f51f2286454b7df1bafb592bd505188ad8240744
Nat Welch <nat@natwelch.com>
2014-08-07 10:00:04 -0700

    Style.

--- app/app.rb
+++ app/app.rb
@@ -13,6 +13,13 @@ module BrainChild
     enable :caching
     set :cache, Padrino::Cache.new(:LRUHash) # Keeps cached values in memory
 
+    ##
+    # Application configuration options.
+    set :dump_errors, true        # Exception backtraces are written to STDERR (default for production/development)
+    set :logging, true            # Logging in STDOUT for development and file for production (default only for development)
+    set :css_asset_folder, 'css'
+    set :js_asset_folder, 'js'
+
     error 404 do
       render 'errors/404'
     end
